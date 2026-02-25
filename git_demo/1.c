f6c9c7fb48c8bcf89177aa3622347d8cfbdf5f69
Chenzhouhui <2691905230@qq.com>
2026-02-25 20:02:57 +0800

    add 1.c

--- git_demo/1.c
+++ git_demo/1.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 int main(void)
 {
-    printf("hello");
+    int a;
+    int *p;
+    a = 10;
+    p = &a;
+    printf("%d  %p\n",a, &a);   
+    printf("%p  %p\n", p, &p);
     return 0;
 }
