9af7fd8ef5da4d4e1004d5506a26c7cd9e6d8ebd
Chenzhouhui <2691905230@qq.com>
2026-03-18 17:46:13 +0800

    add Smarthome

---
+++ Smart_home/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("Welcome to the Smart Home System!\n");
+    /* 器件选型 
+    HC-SR501/人体红外传感器
+    BH1750/光照传感器
+    MQ7/一氧化碳传感器
+    MQ4/甲烷传感器
+    1.44 TFT 彩屏
+    AHT20/温湿度传感器
+    esp8266—01sWifi模块
+    */
+    return 0;
+}
